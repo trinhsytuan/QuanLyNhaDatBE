0d2eff6de3bcd6c7bb3b34c2ab80923b07f0759b
Tuan <trinhsytuan@gmail.com>
2023-08-18 16:44:05 +0700

    Update key server

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const https = require("https");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
 const userModel = require("./src/controllers/User/User");
+const fs = require("fs");
 app.use(express.urlencoded({ extended: true }));
 var cors = require("cors");
 const connection = require("./src/config/database");
 
 app.use(cors({ credentials: true, origin: "*" }));
+//--------Code use for Server, do not remove---------
+
+
+// const options = {
+//   key: fs.readFileSync("server.key"),
+//   cert: fs.readFileSync("server.cert"),
+// };
+// const server = https.createServer(options, app);
+//--------------The end--------------------------------
 app.use("/api/v1/user", userModel);
 
 (async () => {
