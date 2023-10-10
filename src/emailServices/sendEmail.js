a3ff72057946cbd6a6685fe63e0717b070a02a8f
Tuan <trinhsytuan@gmail.com>
2023-10-11 00:43:50 +0700

    Go back gmail

--- src/emailServices/sendEmail.js
+++ src/emailServices/sendEmail.js
@@ -1,10 +1,8 @@
 var nodemailer = require("nodemailer"); // khai báo sử dụng module nodemailer
 require("dotenv").config();
 var transporter = nodemailer.createTransport({
+  service: "Gmail",
   name: "TServices",
-  host: "smtp.sparkpostmail.com",
-  port: "587",
-
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
